67ec4185890711500799a08e2041d7ab8cca33cd
cyleemusic <cyleemusic@gmail.com>
2024-01-13 01:58:46 +0000

    Fix: import entity and module (#10)
    1. import seed entity to seed module.
    2. import seed module to app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SeedModule } from './seed/seed.module';
 import ormConfig from 'ormconfig';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [TypeOrmModule.forRootAsync({ useFactory: () => ormConfig }), SeedModule],
+  imports: [
+    TypeOrmModule.forRootAsync({ useFactory: () => ormConfig }),
+    SeedModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Seed } from './entities/seed.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Seed])],
   controllers: [SeedController],
   providers: [SeedService],
 })
