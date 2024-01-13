ae1bba51041917b1470c78a03b0a9e508b8c3e7d
cyleemusic <cyleemusic@gmail.com>
2024-01-13 01:52:45 +0000

    Add: DTO for create and update seed service (#8)

--- src/seed/dto/create-seed.dto.ts
+++ src/seed/dto/create-seed.dto.ts
@@ -1,1 +1,37 @@
-export class CreateSeedDto {}
+import {
+  IsString,
+  IsNotEmpty,
+  IsBoolean,
+  IsDate,
+  IsArray,
+} from 'class-validator';
+import { SeedType } from '../entities/seed.entity';
+
+export class CreateSeedDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  type: SeedType;
+
+  @IsNotEmpty()
+  @IsString()
+  producer: string;
+
+  @IsNotEmpty()
+  @IsString()
+  origin: string;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  availability: boolean;
+
+  @IsNotEmpty()
+  @IsDate()
+  harvestDate: Date;
+
+  @IsArray()
+  growingConditions: string[];
+}
--- src/seed/dto/update-seed.dto.ts
+++ src/seed/dto/update-seed.dto.ts
@@ -1,4 +1,38 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSeedDto } from './create-seed.dto';
+import {
+  IsString,
+  IsBoolean,
+  IsDate,
+  IsArray,
+  IsOptional,
+} from 'class-validator';
+import { SeedType } from '../entities/seed.entity';
 
-export class UpdateSeedDto extends PartialType(CreateSeedDto) {}
+export class UpdateSeedDto {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  type?: SeedType;
+
+  @IsOptional()
+  @IsString()
+  producer?: string;
+
+  @IsOptional()
+  @IsString()
+  origin?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  availability?: boolean;
+
+  @IsOptional()
+  @IsDate()
+  harvestDate?: Date;
+
+  @IsOptional()
+  @IsArray()
+  growingConditions?: string[];
+}
