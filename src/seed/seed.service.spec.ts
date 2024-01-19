f9fb8e735dbfa53fc8188807e22eb181bb0d9571
cyleemusic <cyleemusic@gmail.com>
2024-01-19 01:30:13 +0000

    Refector: simplify seed api test code (#15)
    - Unified seedId for findOne, update and delete into targetSeedId
    - Use NotFoundException
    - Add NotFoundException test case for update

--- src/seed/seed.service.spec.ts
+++ src/seed/seed.service.spec.ts
@@ -4,12 +4,15 @@ import { CreateSeedDto } from './dto/create-seed.dto';
 import { Seed, SeedType } from './entities/seed.entity';
 import { Repository } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { NotFoundException } from '@nestjs/common';
 import { UpdateSeedDto } from './dto/update-seed.dto';
+import { NotFoundException } from '@nestjs/common';
 
 describe('SeedService', () => {
   let seedService: SeedService;
   let seedRepository: Repository<Seed>;
+
+  const targetSeedId = 1;
+  const nonExistingSeedId = 999;
 
   const mockSeedData: Seed[] = [
     {
@@ -69,9 +72,9 @@ describe('SeedService', () => {
       jest.spyOn(seedRepository, 'create').mockReturnValue(createdSeed);
       jest.spyOn(seedRepository, 'save').mockResolvedValue(createdSeed);
 
-      const result = await seedService.create(createSeedDto);
-
-      expect(result).toEqual(createdSeed);
+      await expect(seedService.create(createSeedDto)).resolves.toEqual(
+        createdSeed,
+      );
       expect(seedRepository.create).toHaveBeenCalledWith(createSeedDto);
       expect(seedRepository.save).toHaveBeenCalledWith(createdSeed);
     });
@@ -81,117 +84,117 @@ describe('SeedService', () => {
     it('should return an array of seeds', async () => {
       jest.spyOn(seedRepository, 'find').mockResolvedValue(mockSeedData);
 
-      const result = await seedService.findAll();
-
-      expect(result).toEqual(mockSeedData);
+      await expect(seedService.findAll()).resolves.toEqual(mockSeedData);
       expect(seedRepository.find).toHaveBeenCalled();
     });
   });
 
   describe('findOne', () => {
     it('should return a seed data', async () => {
-      const targetSeedId = 1;
       const targetSeed: Seed = mockSeedData.find(
         (seed) => seed.id === targetSeedId,
       );
+
       jest.spyOn(seedRepository, 'findOne').mockResolvedValue(targetSeed);
 
-      const result = await seedService.findOne(targetSeedId);
-
-      expect(result).toEqual(targetSeed);
+      await expect(seedService.findOne(targetSeedId)).resolves.toEqual(
+        targetSeed,
+      );
       expect(seedRepository.findOne).toHaveBeenCalledWith({
         where: { id: targetSeedId },
       });
     });
 
-    it('should throw NotFoundException for non-existing seed', async () => {
-      const nonExistingSeedId = 999;
+    it('should throw NotFoundException when finding a non-existing seed', async () => {
       jest.spyOn(seedRepository, 'findOne').mockResolvedValue(null);
 
-      try {
-        await seedService.findOne(nonExistingSeedId);
-        fail();
-      } catch (error) {
-        expect(error).toBeInstanceOf(NotFoundException);
-        expect(error.message).toEqual(
-          `Seed with ID ${nonExistingSeedId} not found`,
-        );
-        expect(seedRepository.findOne).toHaveBeenCalledWith({
-          where: { id: nonExistingSeedId },
-        });
-      }
+      await expect(seedService.findOne(nonExistingSeedId)).rejects.toThrow(
+        new NotFoundException(`Seed with ID ${nonExistingSeedId} not found`),
+      );
+      expect(seedRepository.findOne).toHaveBeenCalledWith({
+        where: { id: nonExistingSeedId },
+      });
     });
   });
 
   describe('update', () => {
+    const updateSeedDTO: UpdateSeedDto = {
+      name: 'updated test seed',
+      type: SeedType.FLOWER,
+      availability: false,
+    };
+    const seedToUpdate: Seed = mockSeedData.find(
+      (seed) => seed.id === targetSeedId,
+    );
     it('should update and return updated seed', async () => {
-      const seedIdToUpdate = 2;
-      const updateSeedDTO: UpdateSeedDto = {
-        name: 'updated test seed',
-        type: SeedType.FLOWER,
-        availability: false,
-      };
-
-      const seedToUpdate: Seed = mockSeedData.find(
-        (seed) => seed.id === seedIdToUpdate,
-      );
-
       jest.spyOn(seedRepository, 'findOne').mockResolvedValue(seedToUpdate);
       jest
         .spyOn(seedRepository, 'save')
         .mockResolvedValue({ ...seedToUpdate, ...updateSeedDTO });
 
-      const result = await seedService.update(seedIdToUpdate, updateSeedDTO);
-
-      expect(result).toMatchObject({
+      await expect(
+        seedService.update(targetSeedId, updateSeedDTO),
+      ).resolves.toMatchObject({
         name: 'updated test seed',
         type: SeedType.FLOWER,
         availability: false,
       });
-
       expect(seedRepository.findOne).toHaveBeenCalledWith({
-        where: { id: seedIdToUpdate },
+        where: { id: targetSeedId },
+      });
+      expect(seedRepository.save).toHaveBeenCalledWith({
+        ...seedToUpdate,
+        ...updateSeedDTO,
+      });
+    });
+    it('should throw NotFoundException when updating a non-existing seed', async () => {
+      jest.spyOn(seedRepository, 'findOne').mockResolvedValue(null);
+      jest.spyOn(seedRepository, 'update');
+
+      await expect(
+        seedService.update(nonExistingSeedId, { ...updateSeedDTO }),
+      ).rejects.toThrow(
+        new NotFoundException(`Seed with ID ${nonExistingSeedId} not found`),
+      );
+      expect(seedRepository.findOne).toHaveBeenCalledWith({
+        where: { id: nonExistingSeedId },
+      });
+      expect(seedRepository.update).not.toHaveBeenCalledWith({
+        id: nonExistingSeedId,
       });
     });
   });
 
   describe('delete', () => {
     it('should delete a seed data', async () => {
-      const seedIdToDelete = 1;
-      jest
-        .spyOn(seedRepository, 'findOne')
-        .mockResolvedValue(
-          mockSeedData.find((seed) => seed.id === seedIdToDelete),
-        );
-
+      jest.spyOn(seedRepository, 'findOne').mockResolvedValue({
+        id: targetSeedId,
+      } as Seed);
       jest.spyOn(seedRepository, 'remove').mockResolvedValue(undefined);
 
-      await seedService.remove(seedIdToDelete);
+      await seedService.remove(targetSeedId);
 
       expect(seedRepository.findOne).toHaveBeenCalledWith({
-        where: { id: seedIdToDelete },
+        where: { id: targetSeedId },
       });
-
-      expect(seedRepository.remove).toHaveBeenCalledWith(
-        mockSeedData.find((seed) => seed.id === seedIdToDelete),
-      );
+      expect(seedRepository.remove).toHaveBeenCalledWith({
+        id: targetSeedId,
+      });
     });
 
-    it('should throw NotFoundException if the seed does not exist', async () => {
-      const nonExistingSeedId = 999;
-
+    it('should throw NotFoundException when removing a non-existing seed', async () => {
       jest.spyOn(seedRepository, 'findOne').mockResolvedValue(null);
-
       jest.spyOn(seedRepository, 'remove');
 
       await expect(seedService.remove(nonExistingSeedId)).rejects.toThrow(
-        `Seed with ID ${nonExistingSeedId} not found`,
+        new NotFoundException(`Seed with ID ${nonExistingSeedId} not found`),
       );
-
       expect(seedRepository.findOne).toHaveBeenCalledWith({
         where: { id: nonExistingSeedId },
       });
-      expect(seedRepository.remove).not.toHaveBeenCalled();
+      expect(seedRepository.remove).not.toHaveBeenCalledWith({
+        id: nonExistingSeedId,
+      });
     });
   });
 });
